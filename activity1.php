003b8232fe65acfb5088a88ac434a908f849328b
Enzxco <lawrencesecjadas@gmail.com>
2025-02-27 15:28:56 +0800

    Update acts

--- activity1.php
+++ activity1.php
@@ -4,13 +4,27 @@ require_once("vendor/autoload.php");
 
 $faker = Faker\Factory::create('en_PH');
 
-for ($i = 0; $i <= 5; $i++) {
-    echo $faker->name . "\n";
-    echo $faker->email . "\n";
-    echo $faker->numerify('+63 9## ### ####') . "\n";
-    echo $faker->address . ' ' . $faker->city . ' ' . $faker->state . "\n";
-    echo $faker->date($format = 'Y-m-d', $max = '2001-12-31') . "\n";
-    echo $faker->jobTitle . "\n";
-}
+echo "<table border='1' spacing= '2' padding='2'>";
+echo "<tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone Number</th>
+        <th>Address</th>
+        <th>Birthdate</th>
+        <th>Job Title</th>
+    </tr>";
+
+    for ($i = 0; $i <= 5; $i++) {
+        echo "<tr>";
+            echo "<td>" . $faker->name . "</td>";
+            echo "<td>" . $faker->email . "</td>";
+            echo "<td>" . $faker->numerify('+63 9## ### ####') . "</td>";
+            echo "<td>" . $faker->address . ', ' . $faker->city . ', ' . $faker->state . "</td>";
+            echo "<td>" . $faker->date($format = 'Y-m-d', $max = '2001-12-31') . "</td>";
+            echo "<td>" . $faker->jobTitle . "</td>";
+        echo "</tr>";
+    }
+
+echo "</table>";
 
 ?>
--- activity2.php
+++ activity2.php
@@ -4,26 +4,28 @@ require_once("vendor/autoload.php");
 
 $faker = Faker\Factory::create('en_PH');
 
-echo "<table border='1' spacing= '2' padding='2'>";
+$genres = ['Fiction', 'Mystery', 'Science Fiction', 'Fantasy', 'Romance', 'Thriller', 'Historical', 'Horror'];
+
+echo "<table border='1'spacing= '2' padding='2'>";
 echo "<tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Phone Number</th>
-        <th>Address</th>
-        <th>Birthdate</th>
-        <th>Job Title</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Genre</th>
+        <th>Publication Year</th>
+        <th>ISBN</th>
+        <th>Summary</th>
     </tr>";
 
-    for ($i = 0; $i <= 5; $i++) {
-        echo "<tr>";
-            echo "<td>" . $faker->name . "</td>";
-            echo "<td>" . $faker->email . "</td>";
-            echo "<td>" . $faker->numerify('+63 9## ### ####') . "</td>";
-            echo "<td>" . $faker->address . ', ' . $faker->city . ', ' . $faker->state . "</td>";
-            echo "<td>" . $faker->date($format = 'Y-m-d', $max = '2001-12-31') . "</td>";
-            echo "<td>" . $faker->jobTitle . "</td>";
-        echo "</tr>";
-    }
+for ($i = 0; $i <= 15; $i++) {
+    echo "<tr>";
+        echo "<td>" . $faker->words(3, true) . "</td>"; 
+        echo "<td>" . $faker->name . "</td>";
+        echo "<td>" . $genres[array_rand($genres)] . "</td>";
+        echo "<td>" . $faker->numberBetween(1900,2024) . "</td>";
+        echo "<td>" . $faker->isbn13 . "</td>";
+        echo "<td>" . $faker->paragraph . "</td>";
+    echo "</tr>";
+}
 
 echo "</table>";
 
